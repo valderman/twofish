d76ab9c4b1bb520038794f351e933b57b444a26f
Anton Ekblad <anton@ekblad.cc>
2013-09-07 11:05:19 +0200

    Compatibility fix for GHC 7.6.
    Since base 4.6.0.0, Num is no longer a requirement for Bits, so we need
    additional Num constraints in Data.Bitlib.

--- Data/Bitlib.hs
+++ Data/Bitlib.hs
@@ -7,14 +7,14 @@ module Data.Bitlib (
 
 import Data.Bits
 
-pack :: (Integral a, Bits a, Bits b) => [a] -> b
+pack :: (Integral a, Bits a, Integral b, Bits b) => [a] -> b
 pack []     = fromInteger 0
 pack (x:[]) = fromIntegral x
 pack (x:xs) = (fromIntegral x) .|. ((pack xs) `shiftL` (bitSize x))
 
 unpack :: (Integral a, Bits a, Num b, Bits b) => a -> [b]
 unpack = doUnpack 0
-    where doUnpack :: (Integral a, Bits a, Bits b) => Int -> a -> [b]
+    where doUnpack :: (Integral a, Bits a, Num b, Bits b) => Int -> a -> [b]
           doUnpack n x
               | n == bitSize x = []
               | otherwise      = (fromIntegral (x .&. 0xFF)) : doUnpack (n + 8) (x `shiftR` 8)
@@ -25,6 +25,6 @@ packMany zero xs@(x:_) =
     let ct = (bitSize zero) `div` (bitSize x)
     in pack (take ct xs) : packMany zero (drop ct xs)
 
-unpackMany :: (Integral a, Bits a, Bits b) => [a] -> [b]
+unpackMany :: (Integral a, Bits a, Num b, Bits b) => [a] -> [b]
 unpackMany []     = []
 unpackMany (x:xs) = concat [(unpack x), (unpackMany xs)]
